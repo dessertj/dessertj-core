158f5e77ee99a91d844dfb990c210e4fbdd033a0
hajo70 <hessmann@spricom.de>
2017-05-06 15:53:49 +0200

    Adds types of referenced fields to dependencies.

--- src/de/spricom/dessert/classfile/ConstantFieldref.java
+++ src/de/spricom/dessert/classfile/ConstantFieldref.java
@@ -1,5 +1,7 @@
 package de.spricom.dessert.classfile;
 
+import java.util.Set;
+
 class ConstantFieldref extends ConstantPoolEntry {
 	public static final int TAG = 9;
 	private final int classIndex;
@@ -22,5 +24,15 @@ class ConstantFieldref extends ConstantPoolEntry {
 
 	public int getNameAndTypeIndex() {
 		return nameAndTypeIndex;
+	}
+
+	@Override
+	protected void addClassNames(Set<String> classNames, ClassFile cf) {
+		ConstantNameAndType nameAndType = (ConstantNameAndType) cf.getConstantPoolEntry(nameAndTypeIndex);
+		ConstantUtf8 descriptor = (ConstantUtf8) cf.getConstantPoolEntry(nameAndType.getDescriptorIndex());
+		FieldType type = new FieldType(descriptor.getValue());
+		if (type.isObjectType()) {
+			classNames.add(type.getObjectTypeClassname());
+		}
 	}
 }
