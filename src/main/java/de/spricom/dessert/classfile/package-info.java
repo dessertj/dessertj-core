8614911d3373dc86ade3db2bd4f255cb49179a4a
hjhessmann <hans.joerg.hessmann@anderscore.com>
2020-12-31 19:40:05 +0100

    Use Maven instead of Gradle

---
+++ src/main/java/de/spricom/dessert/classfile/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the code to anaylze a class file.
+ * The implementation retrieves all the dependencies without anaylizing the byte code.
+ * Hence it's very fast.
+ */
+package de.spricom.dessert.classfile;
