e671fd93b327a4613f928bac0ae644e7c5dfc04b
hajo70 <hessmann@spricom.de>
2021-02-06 10:50:16 +0100

    License added

--- src/test/java/de/spricom/dessert/classfile/SamplesDependenciesTest.java
+++ src/test/java/de/spricom/dessert/classfile/SamplesDependenciesTest.java
@@ -1,9 +1,29 @@
 package de.spricom.dessert.classfile;
 
+/*-
+ * #%L
+ * Dessert Dependency Assertion Library
+ * %%
+ * Copyright (C) 2017 - 2021 Hans Jörg Heßmann
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import de.spricom.dessert.samples.annotations.SpecialArgSample;
 import de.spricom.dessert.samples.generics.EmptyGenericWithBounds;
 import de.spricom.dessert.samples.generics.GenericWithoutBounds;
 import de.spricom.dessert.samples.generics.GenericWithoutBoundsEmpty;
-import de.spricom.dessert.samples.annotations.SpecialArgSample;
 import org.junit.Test;
 
 import java.io.IOException;
